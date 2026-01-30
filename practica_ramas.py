4159882dfe50ad1aa1325ef7d240253e3f2903b8
a130072-lgtm <a130072@idelvalle.org>
2026-01-30 11:55:19 -0600

    Create practica_ramas.py
    Agrega funciones de suma y resta

---
+++ practica_ramas.py
@@ -0,0 +1,12 @@
+def saludar():
+    print("Hola, este es el programa principal")
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+saludar()
+print("Suma:", sumar(5, 3))
+print("Resta:", restar(10, 4))
